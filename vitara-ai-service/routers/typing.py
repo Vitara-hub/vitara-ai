cbacbda15177efdbdfaf5e8dd92f0e6470515b25
@bagus_uswanto <bagoes.1933@gmail.com>
2026-05-22 14:03:49 +0700

    feat: switch to tflite model

--- vitara-ai-service/routers/typing.py
+++ vitara-ai-service/routers/typing.py
@@ -7,7 +7,7 @@ import os
 
 router = APIRouter(prefix="/predict", tags=["Typing"])
 
-MODEL_PATH = "models/typing_model/typing_stress_lstm.h5"
+MODEL_PATH = "models/typing_model/typing_stress_lstm.tflite"
 predictor = None
 
 def load_typing_model():
